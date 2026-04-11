78c43c5b2aa4cebb335d200e21d66cb71ede2573
Ahsan-Qadeer <muhammadahsanqadeer@gmail.com>
2026-04-11 18:27:55 +0500

    Created basic structure for the frontend, aswell as needed files for a React app.

---
+++ frontend/src/pages/Auth.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Auth() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
+        username,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+      alert("Login successful");
+    } catch (err) {
+      console.error(err);
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Auth;
